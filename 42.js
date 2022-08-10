f8849bf55f5e2d8a2d5235c4d2c3a68514ed084f
Joe Johnson <682023+say2joe@users.noreply.github.com>
2022-08-09 19:19:21 -0700

    Update 42.js

--- 42.js
+++ 42.js
@@ -1,23 +1,28 @@
 // Wouldn't this suffice for this repo? What's the rest of the files for?
 
-const fizzBuzz = (num) => {
-    let msg = '';
-    if (num % 3 === 0) msg += 'Fizz';
-    if (num % 5 === 0) msg += 'Buzz';
-    if (msg === '') msg = num;
-    window.alert(msg);
-}
-
-
-// Legacy shorthand (efficient but not proper)
-if (!fizzBuzz) {
-    function fizzBuzz (num) {
-        var msg = '';
-        if (!(num % 3)) msg += 'Fizz';
-        if (!(num % 5)) msg += 'Buzz';
-        if (!msg) msg = num;
+try {
+    
+    const fizzBuzz = (num) => {
+        let msg = '';
+        if (num % 3 === 0) msg += 'Fizz';
+        if (num % 5 === 0) msg += 'Buzz';
+        if (msg === '') msg = num;
         window.alert(msg);
-    }
-};
+    };
+    
+} catch (err) {
+    
+    // Legacy shorthand (efficient but not proper)
+    if (!fizzBuzz) {
+        fizzBuzz = function fizzBuzz (num) {
+            var msg = '';
+            if (!(num % 3)) msg += 'Fizz';
+            if (!(num % 5)) msg += 'Buzz';
+            if (!msg) msg = num;
+            window.alert(msg);
+        };
+    };
+    
+}
 
 fizzBuzz(15);
